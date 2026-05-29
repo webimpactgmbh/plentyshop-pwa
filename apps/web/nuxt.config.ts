b2c15635e3d5f70f0286090af186724549c7d4a6
Mohanathas <office@webimpact.io>
2026-05-29 21:22:33 +0200

    Commit

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -99,6 +99,7 @@ export default defineNuxtConfig({
     '@plentymarkets/shop-core',
     '@plentymarkets/shop-module-mollie',
     '@plentymarkets/shop-module-gtag',
+    'uptain-pwa-beta',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/image',
