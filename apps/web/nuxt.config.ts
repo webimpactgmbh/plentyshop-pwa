8efc2a271b6af9fd8d2efadb3cadd3a5c86c6d04
Thaya <office@webimpact.io>
2026-09-04 16:26:09 +0200

    chore: update Uptain PWA module references and dependencies
    Replaced the deprecated 'uptain-pwa-beta' module with the new '@uptain-gmbh/uptain-pwa' in configuration and package files. Updated the .gitignore to include the new module path and removed references to the old beta version from package-lock.json and package.json, ensuring a cleaner and more organized project structure.

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -99,7 +99,7 @@ export default defineNuxtConfig({
     '@plentymarkets/shop-core',
     '@plentymarkets/shop-module-mollie',
     '@plentymarkets/shop-module-gtag',
-    'uptain-pwa-beta',
+    '@uptain-gmbh/uptain-pwa',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/image',
